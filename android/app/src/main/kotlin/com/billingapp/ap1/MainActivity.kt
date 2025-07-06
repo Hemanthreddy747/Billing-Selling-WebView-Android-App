390319e132ae781929a2b833bf743d9e219d3ab2
hemanth747 <hemanthkumarreddy747@gmail.com>
2025-07-06 16:48:13 +0530

    added to v3

---
+++ android/app/src/main/kotlin/com/billingapp/ap1/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.billingapp.ap1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- android/app/src/main/kotlin/com/example/ap1/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.ap1
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
